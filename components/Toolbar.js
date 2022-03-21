1e051666f4be539461a53d6de8158503cc6dc53e
rikadr <70948427+rikadr@users.noreply.github.com>
2022-03-21 15:17:59 +0100

    Start on toolbar
    added icons folder

--- components/Toolbar.js
+++ components/Toolbar.js
@@ -1,6 +1,34 @@
 function Toolbar() {
+
+    function CreateButton(text, imageURL) {
+            return (
+                <div className="
+            h-20
+            border-2 border-green-600">
+                {text}, {imageURL}
+            </div>
+            )
+    }
+
+
     return (
-        <div>Our toolbar</div>
+        
+        <div className="w-full h-20 flex text-md text-purple-1 bg-purple-5 shadow-sm">
+            
+            {/* Logo for notes */}
+            <div className="
+            w-64 h-full 
+            text-lg text-black flex justify-center items-center
+            border-2 border-orange-600">
+                atCampus notes
+            </div>
+
+            <CreateButton text="Creaty" imageURL="Flashcard.svg" />
+            
+
+
+
+        </div>
     )
 }
 
