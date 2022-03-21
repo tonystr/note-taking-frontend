490a472930355064e6d7531dcf14b97a047fae4c
rikadr <70948427+rikadr@users.noreply.github.com>
2022-03-21 16:26:08 +0100

    top menu buttons, missing logo

--- components/Toolbar.js
+++ components/Toolbar.js
@@ -1,15 +1,26 @@
+import FlashcardIcon from "../icons/Flashcard.svg"
+import QnAIcon from "../icons/Q&A.svg"
+import BlackHole from "../icons/Q&A.svg"
+
+
 function Toolbar() {
 
-    function CreateButton(text, imageURL) {
-            return (
-                <div className="
-            h-20
-            border-2 border-green-600">
-                {text}, {imageURL}
+    function CreateButton({id, text, children }) {
+ 
+        return (
+            <div id={id} className="
+            w-32 h-20 mx-2 py-2
+            text-center text-gray-600 hover:text-white
+            fill-purple-1 hover:bg-purple-1 hover:fill-white hover:shadow-sm
+            border-0 border-green-600">
+                <div className="w-min m-auto">
+                    {children}
+                </div>
+                <div className="border-0 border-green-600">
+                {text}</div>
             </div>
             )
     }
-
 
     return (
         
@@ -19,15 +30,23 @@ function Toolbar() {
             <div className="
             w-64 h-full 
             text-lg text-black flex justify-center items-center
-            border-2 border-orange-600">
-                atCampus notes
+            border-0 border-orange-600">
+                at<b>Campus</b>  notes
             </div>
 
-            <CreateButton text="Creaty" imageURL="Flashcard.svg" />
+            {/* Create buttons */}
+            <CreateButton id="CreateFlashcard" text="Create flashcard">
+                <FlashcardIcon />
+            </CreateButton>
+
+            <CreateButton id="CreateQnA" text="Create QnA" > 
+                <QnAIcon />
+            </CreateButton>
+
+            <CreateButton id="CreateQnA" text="Create Dark Void" > 
+                <QnAIcon />
+            </CreateButton>
             
-
-
-
         </div>
     )
 }
