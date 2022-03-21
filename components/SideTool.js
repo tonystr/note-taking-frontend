1b10367493d3ad16c40c56563ea34177c88adf3e
TonyStr <git@tonystr.net>
2022-03-21 18:04:34 +0100

    Sidetool

---
+++ components/SideTool.js
@@ -0,0 +1,7 @@
+function SideTool({ hidden }) {
+    return (
+        <div className={hidden ? 'hidden' : ''}>hi, im right side</div>
+    );
+}
+
+export default SideTool;
--- pages/noteeditor.js
+++ pages/noteeditor.js
@@ -2,18 +2,21 @@ import { useState } from 'react';
 import Toolbar from '../components/Toolbar';
 import Sidebar from '../components/Sidebar';
 import TextEditor from '../components/TextEditor';
+import SideTool from '../components/SideTool'
 
 function Editor() {
     const [title, setTitle] = useState('Untitled note');
 
     return (
-        <div className='my-12 mx-auto w-1/3 flex flex-col'>
-            <input
-                className='text-dark-2 px-4 text-2xl border-solid border-b-[1px] border-dark-5 outline-none'
-                value={title}
-                onChange={e => setTitle(() => e.target.value)}
-            />
-            <TextEditor />
+        <div className='grow'>
+            <div className='my-12 mx-auto w-1/3 flex flex-col'>
+                <input
+                    className='text-dark-2 px-4 text-2xl border-solid border-b-[1px] border-dark-5 outline-none'
+                    value={title}
+                    onChange={e => setTitle(() => e.target.value)}
+                />
+                <TextEditor />
+            </div>
         </div>
     );
 }
@@ -26,6 +29,7 @@ function NoteEditor() {
             <div className="flex h-full">
                 <Sidebar />
                 <Editor />
+                <SideTool hidden={false} />
             </div>
         </div>
     )
