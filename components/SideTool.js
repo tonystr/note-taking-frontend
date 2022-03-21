64a78799543fe960a2c0c2f854a7ea543d2071ac
TonyStr <git@tonystr.net>
2022-03-21 21:00:31 +0100

    Style flashcard menu

--- components/SideTool.js
+++ components/SideTool.js
@@ -1,4 +1,8 @@
+import { useState } from 'react';
+
 function SideTool({ hidden }) {
+    const [set, setSet] = useState('');
+
     const Grupper = [
         {label: "DATA2500 Operativsystemer", value: 1},
         {label: "DATA2410 Datanettverk og skytjenester", value: 2}
@@ -7,7 +11,7 @@ function SideTool({ hidden }) {
     function FlashcardPreview({front, back}) {
         return (
             <div className="m-2 px-2 py-1 rounded-md hover:bg-purple-4 border-0 border-blue300">
-                
+
                 <p className=" text-sm "> {front}</p>
                 <p className="text-xs text-dark-3">{back}</p>
             </div>
@@ -18,20 +22,24 @@ function SideTool({ hidden }) {
         return (
             <div className="cardinput">
                 <h3 className="heading3Sidemenu">{header}</h3>
-                <textarea id={id} className='w-[280px] m-2 px-2 py-1 rounded-lg' rows='3'></textarea>
+                <textarea id={id} className='w-[280px] m-2 px-2 py-1 rounded-lg border-solid border-[1px] border-purple-4 shadow-sm outline-none focus:border-transparent focus:ring-2 focus:ring-purple-3' rows='3'></textarea>
             </div>
         )
     }
 
-      
-    
     return (
-        <div className={hidden ? 'hidden' : ''}>
-            <div className="w-[320px] h-full px-2 py-1 bg-purple-5 border-l-[1px] border-solid ">
+        <div className='w-[320px]'>
+            <div className={`${hidden ? 'hidden' : ''} h-full px-2 py-1 bg-purple-5 border-l-[1px] border-solid border-dark-6 flex flex-col`}>
                 <h2 className="heading2Sidemenu">Add a new flashcard in:</h2>
-                
+
                 {/* Nedtrekk valg meny */}
-                <select id="cardset-select" className="w-[280px] m-2 px-2 py-1 mb-3" name="Group" >
+                <select
+                    id="cardset-select"
+                    className="w-[280px] m-2 px-2 py-1 mb-3 rounded-md bg-white cursor-pointer border-solid border-2 border-purple-4 truncate"
+                    name="Group"
+                    value={set}
+                    onChange={e => setSet(() => e.target.value)}
+                >
                     <option value="">--Please choose an option--</option>
                     <option value="1">DATA2500 Operativsystemer</option>
                     <option value="2">DATA2410 Datanettverk og skytjenester</option>
@@ -40,19 +48,23 @@ function SideTool({ hidden }) {
                 {/* Input bokser */}
                 <CardInput id="cardfront" header="Card front" />
                 <CardInput id="cardback" header="Card back" />
-                
-                {/* Preview ferdig flashcards i sett */}
-                <h2 className="heading2Sidemenu">Flashcards in XXX</h2>
-                <FlashcardPreview front="Hyperword" back="A way of doing multiple examples with the same word" />
-                <FlashcardPreview front="Test forside forside" back="Test bakside bakside bakside bakside bakside bakside" />
-                <FlashcardPreview front="Test forside forside" back="Test bakside bakside bakside bakside bakside bakside" />
 
+                {/* Preview ferdig flashcards i sett */}
+                {set === '' ? null : (
+                    <div>
+                        <h2 className="heading2Sidemenu">Flashcards in {set}</h2>
+                        <FlashcardPreview front="Hyperword" back="A way of doing multiple examples with the same word" />
+                        <FlashcardPreview front="Test forside forside" back="Test bakside bakside bakside bakside bakside bakside" />
+                        <FlashcardPreview front="Test forside forside" back="Test bakside bakside bakside bakside bakside bakside" />
+
+                    </div>
+                )}
+                <div className='grow' />
                 {/* Knapper */}
-                <div className="px-14 border-0 border-blue500 flex justify-around">
+                <div className="px-14 border-0 my-2 border-blue500 flex justify-around">
                     <button id="flashcardadd" className="w-20 h-10 rounded-md text-white bg-purple-1 hover:bg-purple-2 active:bg-purple-1 " >Add</button>
                     <button id="flashcardcancel" className="w-20 h-10 rounded-md text-purple-1 bg-white border-[1px] hover:border-2 active:border-4 border-purple-1 " >Cancel</button>
                 </div>
-
             </div>
         </div>
     );
