0e008882614d28693b70c91583986610c8a18265
TonyStr <tony.stroemsnaes@gmail.com>
2022-03-22 12:18:09 +0100

    Edit logo

--- components/SideTool.js
+++ components/SideTool.js
@@ -1,36 +1,35 @@
 import { useState } from 'react';
 
+function FlashcardPreview({ front, back }) {
+    return (
+        <div className="m-2 px-2 py-1 rounded-md hover:bg-purple-4 border-0 border-blue300">
+            <p className=" text-sm "> {front}</p>
+            <p className="text-xs text-dark-3">{back}</p>
+        </div>
+    );
+}
+
+function CardInput({ id, header, placeholder, rows=3 }) {
+    return (
+        <div className="cardinput">
+            <h3 className="text-sm text-dark-2 ml-4">{header}</h3>
+            <textarea id={id} placeholder={placeholder} className='w-[280px] m-2 mt-1 px-2 py-1 rounded-lg border-solid border-[1px] border-purple-4 shadow-sm outline-none focus:border-transparent focus:ring-2 focus:ring-purple-3' rows={rows} />
+        </div>
+    );
+}
+
 function SideTool({ visible, hide }) {
     const [set, setSet] = useState('');
 
-    const Grupper = [
+    const grupper = [
         { label: "DATA2500 Operativsystemer", value: 1 },
         { label: "DATA2410 Datanettverk og skytjenester", value: 2 }
-    ]
-
-    function FlashcardPreview({front, back}) {
-        return (
-            <div className="m-2 px-2 py-1 rounded-md hover:bg-purple-4 border-0 border-blue300">
-
-                <p className=" text-sm "> {front}</p>
-                <p className="text-xs text-dark-3">{back}</p>
-            </div>
-        )
-    }
-
-    function CardInput({ id, header, placeholder }) {
-        return (
-            <div className="cardinput">
-                <h3 className="heading3Sidemenu">{header}</h3>
-                <textarea id={id} placeholder={placeholder} className='w-[280px] m-2 px-2 py-1 rounded-lg border-solid border-[1px] border-purple-4 shadow-sm outline-none focus:border-transparent focus:ring-2 focus:ring-purple-3' rows='3'></textarea>
-            </div>
-        )
-    }
+    ];
 
     return (
         <div className='w-[320px]'>
             <div className={`${visible ? '' : 'hidden'} relative h-full px-2 py-1 bg-purple-5 border-l-[1px] border-solid border-dark-6 flex flex-col`}>
-                <button className='absolute right-0 mr-3 text-dark-3' onClick={() => hide()}>X</button>
+                <button className='absolute right-4 top-4 text-dark-3' onClick={() => hide()}>X</button>
                 <h2 className="heading2Sidemenu">Add a new flashcard in:</h2>
 
                 {/* Nedtrekk valg meny */}
@@ -47,20 +46,22 @@ function SideTool({ visible, hide }) {
                 </select>
 
                 {/* Input bokser */}
-                <CardInput id="cardfront" header="Card front" placeholder="Write card front here" />
-                <CardInput id="cardback" header="Card back" placeholder="Write card back here" />
+                <CardInput id="cardfront" header="Question" placeholder="Write a question here" rows={3} />
+                <CardInput id="cardback" header="Answer" placeholder="Write the answer here" rows={4} />
 
                 {/* Preview ferdig flashcards i sett */}
                 {set === '' ? null : (
                     <div>
-                        <h2 className="heading2Sidemenu">Flashcards in {set}</h2>
+                        <h2 className="heading2Sidemenu">Flashcards in {grupper.find(g => g.value === +set)?.label}:</h2>
                         <FlashcardPreview front="Hyperword" back="A way of doing multiple examples with the same word" />
                         <FlashcardPreview front="Test forside forside" back="Test bakside bakside bakside bakside bakside bakside" />
                         <FlashcardPreview front="Test forside forside" back="Test bakside bakside bakside bakside bakside bakside" />
 
                     </div>
                 )}
+
                 <div className='grow' />
+
                 {/* Knapper */}
                 <div className="px-14 border-0 my-2 border-blue500 flex justify-around">
                     <button id="flashcardadd" className="w-20 h-10 rounded-md text-white bg-purple-1 hover:bg-purple-2 active:bg-purple-1 " >Add</button>
--- components/Toolbar.js
+++ components/Toolbar.js
@@ -1,6 +1,6 @@
 import FlashcardIcon from "../icons/Flashcard.svg"
 import QnAIcon from "../icons/Q&A.svg"
-
+import atCampusLogo from '../icons/atcampus_logo_black.png';
 
 function CreateButton({id, text, children, onClick }) {
     return (
@@ -8,7 +8,7 @@ function CreateButton({id, text, children, onClick }) {
             onClick={onClick}
             id={id}
             className="
-            w-32 h-20 mx-2 py-2
+            w-32 h-20 py-2
             text-center text-gray-600 hover:text-white hover:cursor-pointer select-none
             fill-purple-1 hover:bg-purple-1 hover:fill-white hover:shadow-sm
             border-0 border-green-600"
@@ -16,31 +16,34 @@ function CreateButton({id, text, children, onClick }) {
             <div className="w-min m-auto">
                 {children}
             </div>
-            <div className="border-0 border-green-600">
-            {text}</div>
+            <div className="border-0 border-green-600 text-sm">
+                {text}
+            </div>
         </div>
     );
 }
+
 function Toolbar({ viewFlashcardEditor }) {
     return (
         <div className="w-full h-20 flex text-md text-purple-1 bg-purple-5 border-b-[1px] border-dark-6">
 
             {/* Logo for notes */}
             <div className="
-            w-64 h-full mr-5
-            text-lg text-black flex justify-center items-center
-            border-0 border-orange-600">
-
-                <span className="underline">at</span><b>Campus</b> &nbsp;  notes
-
+                w-64 h-full mr-5
+                flex justify-center items-center
+            ">
+                <img src={atCampusLogo.src} width={atCampusLogo.width * .8} alt='' />
+                <span className='ml-[9px] pb-[9px] text-[23px] font-semibold text-dark-1'>
+                    notes
+                </span>
             </div>
 
             {/* Create buttons */}
-            <CreateButton id="CreateFlashcard" text="Create flashcard" onClick={viewFlashcardEditor}>
+            <CreateButton id="CreateFlashcard" text="Flashcard" onClick={viewFlashcardEditor}>
                 <FlashcardIcon />
             </CreateButton>
 
-            <CreateButton id="CreateQnA" text="Create QnA" >
+            <CreateButton id="CreateQnA" text="Question" >
                 <QnAIcon />
             </CreateButton>
 
